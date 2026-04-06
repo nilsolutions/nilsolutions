6c53d7ce645749b00e776df4ba0094d6e004a6b4
nilsolutions <hawkmaiden37@gmail.com>
2026-04-06 13:58:12 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,9 +1,9 @@
 if (getgenv().serenium_LOADED) then 
 	return;
 end;
-info = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId);
-if (info.Creator.CreatorType == "Group" or info.Creator.CreatorTargetId == 5192826) then
+
+if (game.GameId == 1390601379) then
 	loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/86e73bccf9768a31edc644adc20feaf6.lua"))();
-elseif (tostring(game.PlaceId) == "155615604") then
+elseif (game.GameId) == 73885730) then
 	loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/859516ffc2c6719d9fefdc7cdc30b8c6.lua"))();
 end;
