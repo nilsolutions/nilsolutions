c49f98676fa7e739450a1d62562964ff128c37aa
nilsolutions <hawkmaiden37@gmail.com>
2026-04-06 13:59:53 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -4,6 +4,6 @@ end;
 
 if (game.GameId == 1390601379) then
 	loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/86e73bccf9768a31edc644adc20feaf6.lua"))();
-elseif (game.GameId) == 73885730) then
+elseif (game.GameId == 73885730) then
 	loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/859516ffc2c6719d9fefdc7cdc30b8c6.lua"))();
 end;
