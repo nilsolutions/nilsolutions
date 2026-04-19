d2710993d105eac71ce0b5136128b73bb2278dcd
nilsolutions <hawkmaiden37@gmail.com>
2026-04-19 13:55:58 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,7 +3,7 @@ if (getgenv().serenium_LOADED) then
 end;
 
 if (game.GameId == 1390601379) then
-	loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/86e73bccf9768a31edc644adc20feaf6.lua"))();
+	loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/5d835687c2c4a784be7639478e2778f5.lua"))();
 elseif (game.GameId == 73885730) then
 	loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/859516ffc2c6719d9fefdc7cdc30b8c6.lua"))();
 end;
